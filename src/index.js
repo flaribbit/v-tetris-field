e1c49d32b3d8f6b4b2a2947a0cda29f58dc0f034
flaribbit <1149761294@qq.com>
2021-11-26 11:16:52 +0800

    导出组件

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 export { default as VTetrisField } from './VTetrisField.vue'
+export { default as TButton } from './TButton.vue'
+export { default as TFumenPlayer } from './TFumenPlayer.vue'
+export { default as TFumenList } from './TFumenList.vue'
