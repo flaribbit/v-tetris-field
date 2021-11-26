d81c9092533fde7430e7cf4bbc1073b64d7c39e8
flaribbit <1149761294@qq.com>
2021-11-26 10:39:16 +0800

    fumen列表初步完成

---
+++ src/TFumenList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="fumen-list">
+    <VTetrisField v-for="p of pages" :page="p" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+import { decoder } from "tetris-fumen"
+import VTetrisField from "./VTetrisField.vue"
+const props = defineProps({
+  fumen: { type: String, default: "v115@vhAAgH" },
+})
+const pages = ref(decoder.decode(props.fumen))
+</script>
+
+<style>
+.fumen-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+}
+</style>
