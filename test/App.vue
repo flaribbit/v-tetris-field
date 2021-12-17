dc67b9480149e6a8679eb4e3c0792f5dba502c63
flaribbit <1149761294@qq.com>
2021-12-17 10:51:28 +0800

    demo页面支持从url输入 close #2

--- test/App.vue
+++ test/App.vue
@@ -2,13 +2,14 @@
   <h2>TFumenPlayer</h2>
   <TFumenPlayer :fumen="fumen" :page="0" />
   <h2>TFumenList</h2>
-  <TFumenList :fumen="fumen" :height="8" />
+  <TFumenList :fumen="fumen" :height="10" />
 </template>
 
 <script setup lang="ts">
 import TFumenPlayer from '../src/TFumenPlayer.vue';
 import TFumenList from '../src/TFumenList.vue';
-const fumen = "v115@vhPJHYaAkeEfEXoC+BlvlzByEEfE03k2AlP5ABwfAA?A+rQAAqsBsqBvtBTpBVhQeAlvlzByEEfE03k2AlP5ABwvDf?E33ZBBlfbOBV5AAAOfQeAlvlzByEEfE03+2BlP5ABwvDfEV?5k2AlPJVBjzAAA6WQAAzdBpeB0XBXUBFlQnAlvlzByEEfE0?3+2BlP5ABwvDfEXhWEBUYPNBkkuRA1GCLBUupAAdqQnAlvl?zByEEfE038UBlP5ABwvDfEXhWEBUYPNBkkuRA1GCLBU+rAA?AAPAA"
+var fumen = "v115@vhPJHYaAkeEfEXoC+BlvlzByEEfE03k2AlP5ABwfAA?A+rQAAqsBsqBvtBTpBVhQeAlvlzByEEfE03k2AlP5ABwvDf?E33ZBBlfbOBV5AAAOfQeAlvlzByEEfE03+2BlP5ABwvDfEV?5k2AlPJVBjzAAA6WQAAzdBpeB0XBXUBFlQnAlvlzByEEfE0?3+2BlP5ABwvDfEXhWEBUYPNBkkuRA1GCLBUupAAdqQnAlvl?zByEEfE038UBlP5ABwvDfEXhWEBUYPNBkkuRA1GCLBU+rAA?AAPAA"
+if (location.search.length > 0) fumen = location.search.substring(1);
 </script>
 
 <style>
